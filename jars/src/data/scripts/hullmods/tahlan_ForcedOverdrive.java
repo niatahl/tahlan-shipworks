5a086efedc9d3db6dccaf9930134b121429e0880
Nia Tahl <eliyanbrize@gmail.com>
2019-01-17 05:29:06 +0100

    initial public release version (now with files added)

---
+++ jars/src/data/scripts/hullmods/tahlan_ForcedOverdrive.java
@@ -0,0 +1,58 @@
+package data.scripts.hullmods;
+
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShieldAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import data.scripts.tahlan_ModPlugin;
+
+
+public class tahlan_ForcedOverdrive extends BaseHullMod {
+
+    public static final float ARMOR_BONUS = 200f;
+
+    @Override
+    public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
+        stats.getArmorBonus().modifyFlat(id,ARMOR_BONUS);
+        stats.getShieldDamageTakenMult().modifyMult(id, 0f);
+        stats.getShieldUpkeepMult().modifyMult(id, 0f);
+    }
+
+    @Override
+    public void applyEffectsAfterShipCreation(ShipAPI ship, String id) {
+        ship.setShield(ShieldAPI.ShieldType.NONE,0,0,0);
+    }
+
+    @Override
+    public boolean isApplicableToShip(ShipAPI ship) {
+        boolean canBeApplied = ship.getVariant().hasHullMod("tahlan_knightrefit");
+
+        for (String s : ship.getVariant().getHullMods()) {
+            if (tahlan_ModPlugin.SHIELD_HULLMODS.contains(s) && !s.equals("tahlan_forcedoverdrive")) {
+                canBeApplied = false;
+                break;
+            }
+        }
+        return canBeApplied;
+    }
+
+    @Override
+    public String getUnapplicableReason(ShipAPI ship) {
+        if (!ship.getVariant().hasHullMod("tahlan_knightrefit")) {
+            return "Only applicable to Kassadari refits";
+        }
+
+        for (String s : ship.getVariant().getHullMods()) {
+                if (tahlan_ModPlugin.SHIELD_HULLMODS.contains(s) && !s.equals("tahlan_forcedoverdrive")) {
+                    return "Incompatible with shield-related hullmods";
+                }
+        }
+        return null;
+    }
+
+    @Override
+    public String getDescriptionParam(int index, ShipAPI.HullSize hullSize, ShipAPI ship) {
+        if (index == 0) return "permanent Overdrive activation";
+        return null;
+    }
+}
