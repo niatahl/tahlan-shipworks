6e243655da57c49af9f5274e5c5bc115953247e0
NiaTahl <eliyanbrize@gmail.com>
2022-04-08 13:39:52 +0200

    harbinger of your destruction

--- jars/src/data/scripts/hullmods/tahlan_DaemonCore.java
+++ jars/src/data/scripts/hullmods/tahlan_DaemonCore.java
@@ -8,9 +8,13 @@ import com.fs.starfarer.api.combat.listeners.DamageDealtModifier;
 import com.fs.starfarer.api.combat.listeners.DamageTakenModifier;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
 import com.fs.starfarer.api.impl.campaign.ids.Commodities;
+import com.fs.starfarer.api.mission.FleetSide;
+import com.fs.starfarer.api.util.IntervalUtil;
 import com.fs.starfarer.api.util.Misc;
 import org.lazywizard.lazylib.MathUtils;
+import org.lazywizard.lazylib.combat.CombatUtils;
 import org.lwjgl.util.vector.Vector2f;
+import sun.jvm.hotspot.utilities.Interval;
 
 import java.awt.*;
 import java.util.HashMap;
@@ -21,6 +25,7 @@ import static data.scripts.utils.tahlan_Utils.txt;
 public class tahlan_DaemonCore extends BaseHullMod {
 
     private static final Map<HullSize, Integer> MAG = new HashMap<>();
+
     static {
         MAG.put(HullSize.FRIGATE, 2);
         MAG.put(HullSize.DESTROYER, 1);
@@ -31,7 +36,9 @@ public class tahlan_DaemonCore extends BaseHullMod {
     private static final Color JITTER_COLOR = new Color(255, 0, 0, 20);
     private static final Color JITTER_UNDER_COLOR = new Color(255, 0, 0, 80);
 
-    private static final  String dc_id = "tahlan_daemoncore";
+    private static final String dc_id = "tahlan_daemoncore";
+
+    private static final IntervalUtil yoinkTimer = new IntervalUtil(5f, 10f);
 
     @Override
     public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
@@ -53,9 +60,9 @@ public class tahlan_DaemonCore extends BaseHullMod {
         }
 
         if (ship.getVariant().hasHullMod("es_shiplevelHM")) {
-            ship.getMutableStats().getEngineMalfunctionChance().modifyFlat(id,0.1f);
-            ship.getMutableStats().getWeaponMalfunctionChance().modifyFlat(id,0.1f);
-            ship.getMutableStats().getCriticalMalfunctionChance().modifyFlat(id,0.01f);
+            ship.getMutableStats().getEngineMalfunctionChance().modifyFlat(id, 0.1f);
+            ship.getMutableStats().getWeaponMalfunctionChance().modifyFlat(id, 0.1f);
+            ship.getMutableStats().getCriticalMalfunctionChance().modifyFlat(id, 0.01f);
         }
 
         if (!ship.hasListenerOfClass(daemonListener.class)) {
@@ -67,7 +74,7 @@ public class tahlan_DaemonCore extends BaseHullMod {
         }
 
         boolean isPlayerFleet = false;
-        for (FleetMemberAPI member: Global.getSector().getPlayerFleet().getFleetData().getMembersListCopy()) {
+        for (FleetMemberAPI member : Global.getSector().getPlayerFleet().getFleetData().getMembersListCopy()) {
             if (member.getVariant().getHullVariantId().equals(ship.getVariant().getHullVariantId())) {
                 isPlayerFleet = true;
             }
@@ -86,25 +93,36 @@ public class tahlan_DaemonCore extends BaseHullMod {
     @Override
     public void advanceInCombat(ShipAPI ship, float amount) {
 
-        if (ship.getFleetMember().getFleetData() != null) {
-            // no hidden stuff for player
-            if (ship.getFleetMember().getFleetData().getFleet().isPlayerFleet()) {
+        if (Global.getCombatEngine().getFleetManager(ship.getOwner()) == Global.getCombatEngine().getFleetManager(FleetSide.PLAYER)) {
+            //Only run this in campaign context
+            if (!Global.getCombatEngine().isInCampaign()) {
                 return;
             }
+            yoinkTimer.advance(amount);
+            if (yoinkTimer.intervalElapsed()) {
+                for (ShipAPI bote : CombatUtils.getShipsWithinRange(ship.getLocation(), 2000f)) {
+                    if (bote.getHullSpec().getHullId().contains("tahlan_DunScaith_dmn")
+                            && (Math.random() > 0.75f)
+                            && bote.getFleetMember().getFleetCommander().getFaction().getId().contains("legioinfernalis")) {
+                        Global.getCombatEngine().addFloatingText(ship.getLocation(), "ASSUMING DIRECT CONTROL", 40f, Color.RED, ship, 0.5f, 3f);
+                        ship.setOwner(bote.getOwner());
+                    }
+                }
+            }
+        } else {
+
+            if (!ship.isAlive() || ship.isHulk() || ship.isPiece()) {
+                ship.setJitter(dc_id, JITTER_COLOR, 0f, 0, 0f);
+                ship.setJitterUnder(dc_id, JITTER_UNDER_COLOR, 0f, 0, 0f);
+                return;
+            }
+
+            // Enrage function
+            float enrage = 1f + (ship.getHullLevel() * 0.25f);
+            ship.getMutableStats().getTimeMult().modifyMult(dc_id, enrage);
+            ship.setJitter(dc_id, JITTER_COLOR, 1f - ship.getHullLevel(), 3, 5f);
+            ship.setJitterUnder(dc_id, JITTER_UNDER_COLOR, 1f - ship.getHullLevel(), 20, 10f);
         }
-
-        if (!ship.isAlive() || ship.isHulk() || ship.isPiece()) {
-            ship.setJitter(dc_id,JITTER_COLOR,0f,0,0f);
-            ship.setJitterUnder(dc_id,JITTER_UNDER_COLOR,0f,0,0f);
-            return;
-        }
-
-        // Enrage function
-        float enrage = 1f + (ship.getHullLevel() * 0.25f);
-        ship.getMutableStats().getTimeMult().modifyMult(dc_id,enrage);
-        ship.setJitter(dc_id, JITTER_COLOR, 1f-ship.getHullLevel(), 3, 5f);
-        ship.setJitterUnder(dc_id, JITTER_UNDER_COLOR, 1f-ship.getHullLevel(), 20, 10f);
-
     }
 
     @Override
@@ -150,10 +168,10 @@ public class tahlan_DaemonCore extends BaseHullMod {
             person = Misc.getAICoreOfficerPlugin(Commodities.GAMMA_CORE).createPerson(Commodities.GAMMA_CORE, "tahlan_legioinfernalis", Misc.random);
         } else if (die == 2) {
             person = Misc.getAICoreOfficerPlugin(Commodities.BETA_CORE).createPerson(Commodities.BETA_CORE, "tahlan_legioinfernalis", Misc.random);
-            member.getStats().getDynamic().getMod("individual_ship_recovery_mod").modifyFlat("tahlan_daemoncore",-100f);
+            member.getStats().getDynamic().getMod("individual_ship_recovery_mod").modifyFlat("tahlan_daemoncore", -100f);
         } else {
             person = Misc.getAICoreOfficerPlugin(Commodities.ALPHA_CORE).createPerson(Commodities.ALPHA_CORE, "tahlan_legioinfernalis", Misc.random);
-            member.getStats().getDynamic().getMod("individual_ship_recovery_mod").modifyFlat("tahlan_daemoncore",-1000f);
+            member.getStats().getDynamic().getMod("individual_ship_recovery_mod").modifyFlat("tahlan_daemoncore", -1000f);
         }
         member.setCaptain(person);
     }
@@ -184,9 +202,9 @@ public class tahlan_DaemonCore extends BaseHullMod {
             WeaponAPI weapon;
             if (param instanceof DamagingProjectileAPI) {
                 weapon = ((DamagingProjectileAPI) param).getWeapon();
-            } else if ( param instanceof BeamAPI ){
+            } else if (param instanceof BeamAPI) {
                 weapon = ((BeamAPI) param).getWeapon();
-            } else if ( param instanceof WeaponAPI ) {
+            } else if (param instanceof WeaponAPI) {
                 weapon = (WeaponAPI) param;
             } else {
                 return null;
@@ -214,15 +232,15 @@ public class tahlan_DaemonCore extends BaseHullMod {
             ShipAPI enemy = (ShipAPI) target;
             if (enemy.getVariant().hasHullMod("es_shiplevelHM")) {
                 // Clown mod gets clown damage
-                damage.setDamage(damage.getDamage()*MathUtils.getRandomNumberInRange(1f,5f));
+                damage.setDamage(damage.getDamage() * MathUtils.getRandomNumberInRange(1f, 5f));
                 return null;
             }
             if (enemy.getHullSpec().getHullId().contains("MSS_") || enemy.getVariant().hasHullMod("CHM_mayasura") || enemy.getHullSpec().getHullId().contains("missp_")) {
-                damage.setDamage(damage.getDamage()*1.5f);
+                damage.setDamage(damage.getDamage() * 1.5f);
                 return null;
             }
             if (enemy.getHullSpec().getHullId().contains("tesseract") || enemy.getHullSpec().getHullId().contains("facet") || enemy.getHullSpec().getHullId().contains("shard_")) {
-                damage.setDamage(damage.getDamage()*2f);
+                damage.setDamage(damage.getDamage() * 2f);
                 return null;
             }
             return null;
