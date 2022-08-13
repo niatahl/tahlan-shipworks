20b726e5f159da681a5dfc9f90c3840fd8339a38
Nia Tahl <eliyanbrize@gmail.com>
2022-08-13 16:20:29 +0200

    Update 0.8.5

--- .github/workflows/runner.sh
+++ .github/workflows/runner.sh
@@ -2,7 +2,7 @@
 
 ### Edit
 # Your mod name. The version number will be attached to this to form "My-Mod-1.0.0"
-MOD_FOLDER_NAME=$(head -n 1 './mod-folder-name.txt')
+MOD_FOLDER_NAME=tahlan-shipworks
 echo "Folder name without version will be $MOD_FOLDER_NAME"
 ###
 
