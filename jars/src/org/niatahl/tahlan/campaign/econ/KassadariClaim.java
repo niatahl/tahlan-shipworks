1e6faf73aaf35534a727ae6b96b08ec2d9c6f98b
NiaTahl <eliyanbrize@gmail.com>
2023-03-22 12:24:37 +0100

    claim

--- jars/src/org/niatahl/tahlan/campaign/econ/KassadariClaim.java
+++ jars/src/org/niatahl/tahlan/campaign/econ/KassadariClaim.java
@@ -12,12 +12,14 @@ import static org.niatahl.tahlan.utils.Utils.txt;
 
 public class KassadariClaim extends BaseMarketConditionPlugin implements MarketImmigrationModifier {
 
-    public static float ACCESSIBILITY_BONUS = -50f;
+    public static final float ACCESSIBILITY_BONUS = -100f;
+    public static final float HAZARD_BONUS = 50f;
 
     @Override
     public void apply(String id) {
         super.apply(id);
-        market.getAccessibilityMod().modifyFlat(id, getAccessibilityBonus()/100, txt("econ_claim1"));
+        market.getAccessibilityMod().modifyFlat(id, ACCESSIBILITY_BONUS/100, txt("econ_claim1"));
+        market.getHazard().modifyFlat(id,HAZARD_BONUS/100, txt("econ_claim1"));
         market.addTransientImmigrationModifier(this);
     }
 
@@ -36,10 +38,6 @@ public class KassadariClaim extends BaseMarketConditionPlugin implements MarketI
         return -2*market.getSize();
     }
 
-    private float getAccessibilityBonus(){
-        return Math.max(0, ACCESSIBILITY_BONUS-(Math.max(0, market.getSize()-3)));
-    }
-
     @Override
     protected void createTooltipAfterDescription(TooltipMakerAPI tooltip, boolean expanded) {
         super.createTooltipAfterDescription(tooltip, expanded);
@@ -47,6 +45,10 @@ public class KassadariClaim extends BaseMarketConditionPlugin implements MarketI
         tooltip.addPara(txt("access"),
                 10f, Misc.getHighlightColor(),
                 ""+(int)ACCESSIBILITY_BONUS+"%"
+        );
+        tooltip.addPara(txt("+") + txt("hazard"),
+                10f, Misc.getHighlightColor(),
+                ""+(int)HAZARD_BONUS+"%"
         );
         tooltip.addPara(
                 txt("econ_claim2"),
