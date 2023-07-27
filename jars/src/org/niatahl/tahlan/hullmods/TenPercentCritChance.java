4aa81db3480118bc4c9dfc9f146f4400a072f7aa
NiaTahl <eliyanbrize@gmail.com>
2023-07-27 16:34:22 +0200

    better critfix

--- jars/src/org/niatahl/tahlan/hullmods/TenPercentCritChance.java
+++ jars/src/org/niatahl/tahlan/hullmods/TenPercentCritChance.java
@@ -11,40 +11,42 @@ import org.lwjgl.util.vector.Vector2f;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class TenPercentCritChance extends BaseHullMod {
 
     private static final float CRIT_CHANCE = 0.1f;
     private static final float CRIT_MULT = 3f;
 
-    private final List<DamagingProjectileAPI> toCrit = new ArrayList<>();
-    private final List<DamagingProjectileAPI> hasHit = new ArrayList<>();
-
     @Override
     public void advanceInCombat(ShipAPI ship, float amount) {
 
-        if (Global.getCombatEngine().isCombatOver()) {
-            toCrit.clear();
-            hasHit.clear();
-            return;
+        String id = ship.getId();
+        Data data;
+        Map<String, Object> customCombatData = Global.getCombatEngine().getCustomData();
+        if (customCombatData.get("tahlan_crit" + id) instanceof TenPercentCritChance.Data)
+            data = (TenPercentCritChance.Data) customCombatData.get("tahlan_crit" + id);
+        else {
+            data = new TenPercentCritChance.Data();
+            customCombatData.put("tahlan_crit" + id, data);
         }
 
         if (!ship.isHulk() && !ship.isPiece()) {
 
             for (DamagingProjectileAPI proj : CombatUtils.getProjectilesWithinRange(ship.getLocation(), 500f)) {
-                if (ship == proj.getSource() && !toCrit.contains(proj) && !hasHit.contains(proj)) {
+                if (ship == proj.getSource() && !data.toCrit.contains(proj) && !data.hasHit.contains(proj)) {
                     if (Math.random() < CRIT_CHANCE) {
-                        toCrit.add(proj);
+                        data.toCrit.add(proj);
                         proj.setDamageAmount(proj.getDamageAmount()*CRIT_MULT);
                     } else {
-                        hasHit.add(proj);
+                        data.hasHit.add(proj);
                     }
                 }
             }
 
-            for (DamagingProjectileAPI proj : toCrit) {
-                if (proj.didDamage() && !hasHit.contains(proj)) {
-                    hasHit.add(proj);
+            for (DamagingProjectileAPI proj : data.toCrit) {
+                if (proj.didDamage() && !data.hasHit.contains(proj)) {
+                    data.hasHit.add(proj);
                     if (proj.getDamageTarget() instanceof ShipAPI) {
                         Global.getCombatEngine().addFloatingText(proj.getLocation(), "CRIT!", 30f, Color.red, proj, 5f, 1f);
                         Global.getCombatEngine().addHitParticle(proj.getLocation(), proj.getVelocity(), 100f, 1f, 0.05f, Color.white);
@@ -53,17 +55,22 @@ public class TenPercentCritChance extends BaseHullMod {
             }
 
             final List<DamagingProjectileAPI> toRemove = new ArrayList<>();
-            for (DamagingProjectileAPI proj : hasHit) {
+            for (DamagingProjectileAPI proj : data.hasHit) {
                 if (!Global.getCombatEngine().isEntityInPlay(proj)) {
-                    toCrit.remove(proj);
+                    data.toCrit.remove(proj);
                     toRemove.add(proj);
                 }
             }
 
             for (DamagingProjectileAPI proj : toRemove) {
-                hasHit.remove(proj);
+                data.hasHit.remove(proj);
             }
             toRemove.clear();
         }
+    }
+
+    private static class Data {
+        final List<DamagingProjectileAPI> toCrit = new ArrayList<>();
+        final List<DamagingProjectileAPI> hasHit = new ArrayList<>();
     }
 }
